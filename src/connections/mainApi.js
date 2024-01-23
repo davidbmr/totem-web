17b953030bae10669d6fd42471ed46a94fd600bd
David Machuca <davidmachuca.dev@gmail.com>
2024-01-23 18:59:29 -0500

    url

--- src/connections/mainApi.js
+++ src/connections/mainApi.js
@@ -1,4 +1,4 @@
-const first_server = "https://api.totem.merakioffice.com/";
+const first_server = "https://api.totem.merakioffice.com";
 const second_server = "http://84.46.241.5:3001/api/v1";
 
 const urls = [first_server, second_server];
